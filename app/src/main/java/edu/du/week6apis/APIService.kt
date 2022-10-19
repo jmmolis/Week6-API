37bae5142abb18c6acc55e27f5486f6fe808d6e7
Jennifer Molis <jmmolis@Pittmo-MacBook-Pro.local>
2022-10-19 12:09:07 -0600

    Update comments

--- app/src/main/java/edu/du/week6apis/APIService.kt
+++ app/src/main/java/edu/du/week6apis/APIService.kt
@@ -14,12 +14,10 @@ interface APIService {
     @POST("/api/v1/create")
     suspend fun createDog(@Body requestBody: RequestBody): Response<ResponseBody>
 
-
     // Form Data
     @Multipart
     @POST("/post")
     suspend fun uploadDogData(@PartMap map: HashMap<String?, RequestBody?>): Response<ResponseBody>
-
 
     // Encoded URL
     @FormUrlEncoded
@@ -27,18 +25,14 @@ interface APIService {
     suspend fun createDog(@FieldMap params: HashMap<String?, String?>): Response<ResponseBody>
 
 
-
-
     /* GET METHOD */
 
     @GET("/api/v1/dogs")
     suspend fun getDogs(): Response<ResponseBody>
 
-
     // Request using @Query (e.g https://reqres.in/api/dogs?page=2)
     @GET("/api/dogs")
     suspend fun getDogs(@Query("page") page: String?): Response<ResponseBody>
-
 
     // Request using @Path (e.g https://reqres.in/api/dogs/53 - This URL is just an example, it's not working)
     @GET("/api/dogs/{Id}")
@@ -51,9 +45,7 @@ interface APIService {
 
     /* DELETE METHOD - when dog is adopted */
 
-    @DELETE("/typicode/demo/posts/1")
+    @DELETE("/typicode/jmmolis/Week6-API/1")
     suspend fun deleteDog(): Response<ResponseBody>
-
-
 
 }
